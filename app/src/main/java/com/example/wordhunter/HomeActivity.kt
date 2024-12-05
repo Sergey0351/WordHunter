0ad7b9b4212f96a0c39a17a4ac629f91756cf8f9
rudnev <ilyarudnev16@gmail.com>
2024-12-06 02:31:27 +0300

    Добавлен navigation bar, checkpoint - HomeActivity.kt

--- app/src/main/java/com/example/wordhunter/HomeActivity.kt
+++ app/src/main/java/com/example/wordhunter/HomeActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.wordhunter.databinding.ActivityMainBinding
+import com.example.wordhunter.databinding.HomeActivityBinding
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var binding: HomeActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = HomeActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Check point
+        binding.bNav.setOnNavigationItemSelectedListener {
+            true
         }
+        //
     }
 }

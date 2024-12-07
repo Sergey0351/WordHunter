5b04b9af6c08e6de6c586dd44eadaa176797a7e1
rudnev <ilyarudnev16@gmail.com>
2024-12-08 01:39:52 +0300

    Добавлен переход через nav_bar

--- app/src/main/java/com/example/wordhunter/HomeActivity.kt
+++ app/src/main/java/com/example/wordhunter/HomeActivity.kt
@@ -5,6 +5,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.replace
 import com.example.wordhunter.databinding.ActivityMainBinding
 import com.example.wordhunter.databinding.HomeActivityBinding
 
@@ -15,10 +18,29 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = HomeActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(Home())
         // Check point
-        binding.bNav.setOnNavigationItemSelectedListener {
+        binding.bNav.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.Home -> replaceFragment(Home())
+                R.id.Profile -> replaceFragment(Profile())
+                R.id.Favorite -> replaceFragment(Favorite())
+
+                else ->{
+
+                }
+            }
             true
         }
-        //
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager =  supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
